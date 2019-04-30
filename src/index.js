4a69fff611ce8330b92abbbd4d1e0976345db527
damienteo <ksteo89@gmail.com>
2019-04-30 19:58:59 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+// function getButtonText() {
+//   return "Click on me!";
+// }
+
 const App = () => {
-  return <div>Hi there!</div>;
+  const buttonText = "Click Me!";
+  return (
+    <div>
+      <label className="label" htmlFor="name">
+        Enter name:
+      </label>
+      <input id="name" type="text" />
+      <button style={{ backgroundColor: "blue", color: "white" }}>
+        {buttonText}
+      </button>
+    </div>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
