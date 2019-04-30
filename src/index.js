c7578f5a587076c30c70fc6bd2956cb815405e6c
damienteo <ksteo89@gmail.com>
2019-04-30 20:13:01 +0800

    Used semantics UI

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// function getButtonText() {
-//   return "Click on me!";
-// }
-
 const App = () => {
   const buttonText = "Click Me!";
   return (
-    <div>
-      <label className="label" htmlFor="name">
-        Enter name:
-      </label>
-      <input id="name" type="text" />
-      <button style={{ backgroundColor: "blue", color: "white" }}>
-        {buttonText}
-      </button>
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" />
+        </a>
+        <div className="content">
+          <a href="/" classname="author">
+            Sam
+          </a>
+          <div className="metadata">
+            <span classNam="date">Today at 6:00pm</span>
+          </div>
+          <div className="text">Nice blogpost</div>
+        </div>
+      </div>
     </div>
   );
 };
